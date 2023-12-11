6e793acf8293dec7cdef49bd219811c7a92a940a
JpDevs <71285497+JpDevs@users.noreply.github.com>
2023-12-11 17:46:53 -0300

    chore: change status int to str on voucher
    chore: change status int to str on voucher

--- public/js/voucher.js
+++ public/js/voucher.js
@@ -2,6 +2,15 @@ function getItem(id) {
     $(document).ready(function () {
         $.get('/api/bookings/' + id + '/edit', function (data) {
             let booking = data.booking
+            let status = {
+                1: "Pendente",
+                2: "Confirmada",
+                3: "Recuperada",
+                0: "Cancelada"
+            }
+
+            let currentStatus = booking.status
+
             $('#booking_holder').val(booking.booking_holder)
             $('#holder_email').val(booking.holder_email)
             $('#holder_phone').val(booking.holder_phone)
@@ -9,7 +18,7 @@ function getItem(id) {
             $('#check_out').val(booking.check_out)
             $('#kids').val(booking.kids)
             $('#adults').val(booking.adults)
-            $('#status').val(booking.status)
+            $('#status').val(status[currentStatus])
             $('#option' + booking.status).attr('selected', true)
             $('#booking_code').text('Reserva ' + booking.booking_code)
         });
